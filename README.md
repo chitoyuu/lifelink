59bc61a92de45de9566400f7ebd3da4e339a7720
Chitose Yuuzaki <chitoyuu@potatoes.gay>
2021-10-06 08:16:31 +0000

    Add tests and documentation

---
+++ README.md
@@ -0,0 +1,124 @@
+# lifelink
+
+Erase covariant lifetime parameters from anything, with generic associated types.
+
+```toml
+[dependencies]
+lifelink = { version = "0.1.0", features = ["nightly"] }
+```
+
+Like `cryo`, `lifelink` allows you to use the resulting types in dynamic environments where lifetime is unpredictable, like runtimes of garbage-collected scripting languages, or where `Any` is required. Unlike `cryo`, the interface is not restricted to primitive references: it works on everything with covariant lifetime parameters though GATs.
+
+**Requires a nightly compiler as of Oct. 2021**, due to the use of `feature(generic_associated_types)`.
+
+## Examples
+
+Simple case with just a reference:
+
+```rust
+#![feature(generic_associated_types)]
+
+use std::thread::spawn;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use lifelink::{Lifelink, RefCtor};
+
+let answer = AtomicUsize::new(0);
+
+let (mut lifelink, deathtouch) = Lifelink::<RefCtor<AtomicUsize>>::new(&answer);
+
+{
+    let guard = lifelink.get().unwrap();
+    assert_eq!(0, guard.load(Ordering::Relaxed));
+    guard.store(42, Ordering::Release);
+}
+
+assert_eq!(42, deathtouch.unwrap().load(Ordering::Acquire));
+```
+
+A more involved example with multiple lifetime parameters, unrelated type parameters. and threads:
+
+```rust
+#![feature(generic_associated_types)]
+
+use std::thread::spawn;
+use std::time::Duration;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::mpsc::channel;
+use std::marker::PhantomData;
+use lifelink::{Lifelink, Ctor, Cov};
+
+struct Answers<'a, 'b, 'c, T> {
+    first: &'a AtomicUsize,
+    second: &'b AtomicUsize,
+    third: &'c AtomicUsize,
+    rest: T,
+}
+
+struct AnswersCtor<T> {
+    _marker: PhantomData<T>,
+}
+
+impl<T: 'static> Ctor for AnswersCtor<T> {
+    // The lifetimes can be unified here, due to covariance
+    type Ty<'a> = Answers<'a, 'a, 'a, T>;
+}
+
+// Although this impl is `unsafe`, the requirement of a `cov` implementation
+// still allows Rust to prevent human mistakes. See the Caveats section for
+// more details.
+unsafe impl<T: 'static> Cov for AnswersCtor<T> {
+    fn cov<'r, 'a, 'b>(r: &'r Self::Ty<'a>) -> &'r Self::Ty<'b>
+    where
+        'a: 'b,
+    {
+        r
+    }
+}
+
+fn compute<'a, 'b, 'c>(answers: Answers<'a, 'b, 'c, ()>) {
+    let (mut lifelink, deathtouch) = Lifelink::<AnswersCtor<()>>::new(answers);
+    let (send, recv) = channel();
+    
+    spawn(move || {
+        let guard = lifelink.get().unwrap();
+        guard.first.store(42, Ordering::Release);
+        guard.second.store(42, Ordering::Release);
+        guard.third.store(42, Ordering::Release);
+        send.send(()).unwrap();
+    });
+
+    // Unlike `cryo`, `lifelink` does *not* attempt to wait until the `'static`
+    // handle is dropped. As such, a way to wait for task completion external
+    // to `lifelink` is required. See the Caveats section of README for more
+    // details, and the rationale behind this decision.
+    recv.recv_timeout(Duration::from_millis(20)).unwrap();
+
+    let answers = deathtouch.unwrap();
+    assert_eq!(42, answers.first.load(Ordering::Acquire));
+    assert_eq!(42, answers.second.load(Ordering::Acquire));
+    assert_eq!(42, answers.third.load(Ordering::Acquire));
+}
+
+let first = AtomicUsize::new(0);
+let second = AtomicUsize::new(0);
+let third = AtomicUsize::new(0);
+
+compute(Answers {
+    first: &first,
+    second: &second,
+    third: &third,
+    rest: (),
+});
+```
+
+## Caveats
+
+`Lifelink` can only ever give out shared / immutable references. This is because Rust allows moves by default, making mutable references to types with lifetime parameters too hard to reason about, and almost impossible to use correctly unless reduced to uselessness. Instead, users have to use interior mutability in a way that maintains covariance (which, thankfully, Rust will help prove in a `Cov` impl).
+
+Unlike `cryo`, `lifelink` does *not* attempt to wait until the `'static` handle is dropped. It's more than happy to drop or unwrap a `Deathtouch`, if there isn't a `Guard` in scope somewhere that *precise moment*. This may come as surprising, but is a conscious decision to make `lifelink` work in tandem with environments where lifetime is unpredictable, e.g. a garbage collected scripting language, where it's much better to get a error than a deadlock from a misbehaving script. As such, a way to wait for task completion external to `lifelink` is required.
+
+Ideally, users shouldn't have to provide their own `unsafe` `Cov` implementations. However, there is no way to assert covariance at the type level in Rust today. As such, the burden to prove covariance have to be passed on to the users.
+
+## Feature flags
+
+- `nightly` - Adds `feature(generic_associated_types)` to the top of the crate, which would allow the crate to compile on a nightly compiler. **Required** as of Oct. 2021.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+#![doc = include_str!("../README.md")]
 #![cfg_attr(feature = "nightly", feature(generic_associated_types))]
 
+use std::cell::Cell;
 use std::marker::PhantomData;
 use std::ops::Deref;
 use std::ptr::null_mut;
@@ -9,25 +11,75 @@ use parking_lot::{Mutex, MutexGuard};
 
 type Inner = Arc<Mutex<*mut u8>>;
 
+/// A `'static` handle through which a value with a covariant lifetime parameter can
+/// temporarily be accessed.
+///
+/// `Lifelink<C>` implements [`Send`] and [`Sync`] when the wrapped values produced by
+/// `C` are [`Send`].
+///
+/// See [`Lifelink::new`] for an example.
 pub struct Lifelink<C: Ctor> {
     inner: Inner,
     _marker: PhantomData<C::Ty<'static>>,
 }
 
+// SAFETY: Inner is a Mutex, and `get` requires &mut self, so it's impossible to obtain
+// multiple references to a Send-only type by Sending the Lifelink itself.
 unsafe impl<C: SendCtor> Send for Lifelink<C> {}
 unsafe impl<C: SendCtor> Sync for Lifelink<C> {}
 
-impl<C: Ctor> Clone for Lifelink<C> {
-    fn clone(&self) -> Self {
-        Lifelink {
-            inner: Arc::clone(&self.inner),
-            _marker: PhantomData,
-        }
-    }
-}
-
 impl<C: Ctor> Lifelink<C> {
-    pub fn get(&self) -> Option<Guard<'_, C>> {
+    /// Create a pair of [`Lifelink`] and [`Deathtouch`] values wrapping `thing`, which
+    /// will be kept alive and accessible through `Lifelink` until the [`Deathtouch`]
+    /// is dropped.
+    ///
+    /// Calling [`Lifelink::new`] is safe as long as `C::Ty` values are covariant over the
+    /// lifetime parameter.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// #![feature(generic_associated_types)]
+    ///
+    /// use std::thread::spawn;
+    /// use std::sync::atomic::{AtomicUsize, Ordering};
+    /// use lifelink::{Lifelink, RefCtor};
+    ///
+    /// let answer = AtomicUsize::new(0);
+    ///
+    /// let (mut lifelink, deathtouch) = Lifelink::<RefCtor<AtomicUsize>>::new(&answer);
+    ///
+    /// {
+    ///     let guard = lifelink.get().unwrap();
+    ///     assert_eq!(0, guard.load(Ordering::Relaxed));
+    ///     guard.store(42, Ordering::Release);
+    /// }
+    ///
+    /// assert_eq!(42, deathtouch.unwrap().load(Ordering::Acquire));
+    /// ```
+    #[allow(clippy::needless_lifetimes)] // False positive
+    pub fn new<'a>(thing: C::Ty<'a>) -> (Lifelink<C>, Deathtouch<'a, C>) {
+        let thing = Box::new(thing);
+        let ptr = Box::into_raw(thing) as *mut u8;
+        let inner = Arc::new(Mutex::new(ptr));
+
+        let lifelink = Lifelink {
+            inner: Arc::clone(&inner),
+            _marker: PhantomData,
+        };
+
+        let deathtouch = Deathtouch {
+            inner,
+            _marker: PhantomData,
+        };
+
+        (lifelink, deathtouch)
+    }
+
+    /// Returns a guard value that implements [`Deref`] to the wrapped value. Care must be taken
+    /// to ensure that this value is dropped properly. Leaking the guard value may lead to a
+    /// deadlock.
+    pub fn get(&mut self) -> Option<Guard<'_, C>> {
         let lock = self.inner.lock();
         if lock.is_null() {
             None
@@ -40,11 +92,19 @@ impl<C: Ctor> Lifelink<C> {
     }
 }
 
+/// A guard value that provides temporary access to the wrapped value. Care must be taken
+/// to ensure that this value is dropped properly. Leaking the guard value may lead to a
+/// deadlock.
+///
+/// `Guard<'_, C>` implements [`Send`] and [`Sync`] when the wrapped values produced by
+/// `C` are [`Sync`].
 pub struct Guard<'a, C: Ctor> {
     lock: MutexGuard<'a, *mut u8>,
     _marker: PhantomData<C::Ty<'static>>,
 }
 
+// SAFETY: Shared references are Send / Sync when and only when the value referred to is
+// Sync.
 unsafe impl<'a, C: SyncCtor> Send for Guard<'a, C> {}
 unsafe impl<'a, C: SyncCtor> Sync for Guard<'a, C> {}
 
@@ -60,42 +120,57 @@ where
     }
 }
 
+/// A guard value that preserves compile-time lifetime information from the value passed
+/// to [`Lifelink::new`].
+///
+/// This will block the calling thread when unwrapped or dropped, until all [`Guard`]s
+/// currently alive go out of the scope. The value can then no longer be accessed from the
+/// corresponding [`Lifelink`].
+///
+/// Note that Unlike `cryo`, this does *not* attempt to wait until the [`Lifelink`] handle
+/// itself is dropped, only the [`Guard`] values it produces.  See the Caveats section of
+/// README for more details.
 pub struct Deathtouch<'a, C: Ctor> {
     inner: Inner,
-    _marker: PhantomData<C::Ty<'a>>,
+
+    /// Invariant over 'a
+    _marker: PhantomData<Cell<&'a C::Ty<'a>>>,
+}
+
+// SAFETY: The only public interfaces of this type takes `self`, and waits until there are
+// no other Guards alive.
+unsafe impl<'a, C: SendCtor> Send for Deathtouch<'a, C> {}
+
+impl<'a, C: Ctor> Deathtouch<'a, C> {
+    fn extract(&mut self) -> Option<C::Ty<'a>> {
+        let mut lock = self.inner.lock();
+        let ptr: *mut u8 = std::mem::replace(&mut *lock, null_mut());
+        if ptr.is_null() {
+            None
+        } else {
+            // SAFETY: this pointer is produced in Lifelink::new with Box::into_raw, and
+            // the Deathtouch type itself is invariant over 'a
+            unsafe { Some(*Box::from_raw(ptr as *mut C::Ty<'a>)) }
+        }
+    }
+
+    /// Unwrap the contained value and return it. This will block until there is no
+    /// other references to the value, so that compile-time lifetime invariants hold.
+    ///
+    /// See the module-level documentation for examples.
+    pub fn unwrap(mut self) -> C::Ty<'a> {
+        self.extract().unwrap()
+    }
 }
 
 impl<'a, C: Ctor> Drop for Deathtouch<'a, C> {
     fn drop(&mut self) {
-        let mut lock = self.inner.lock();
-        let ptr: *mut u8 = std::mem::replace(&mut *lock, null_mut());
-        unsafe { drop(Box::from_raw(ptr as *mut C::Ty<'a>)) };
+        self.extract();
     }
 }
 
-/// Create a pair of `Lifelink` and `Deathtouch` values wrapping `thing`, which
-/// will be kept alive and accessible through `Lifelink` until the `Deathtouch` is dropped.
-#[allow(clippy::needless_lifetimes)] // False positive
-pub fn link<'a, C: Ctor>(thing: C::Ty<'a>) -> (Lifelink<C>, Deathtouch<'a, C>) {
-    let thing = Box::new(thing);
-    let ptr = Box::into_raw(thing) as *mut u8;
-    let inner = Arc::new(Mutex::new(ptr));
-
-    let lifelink = Lifelink {
-        inner: Arc::clone(&inner),
-        _marker: PhantomData,
-    };
-
-    let deathtouch = Deathtouch {
-        inner,
-        _marker: PhantomData,
-    };
-
-    (lifelink, deathtouch)
-}
-
 /// Trait for type constructors that take one single lifetime parameter. See also
-/// [`Cov`] for `Deref` on `Guard`.
+/// [`Cov`] for [`Deref`] on [`Guard`].
 pub trait Ctor {
     type Ty<'a>;
 }
@@ -107,7 +182,8 @@ pub trait Ctor {
 /// to `cov` and letting Rust figure out the rest:
 ///
 /// ```rust
-/// #use lifelink::*;
+/// #![feature(generic_associated_types)]
+/// # use lifelink::*;
 ///
 /// struct FooCtor;
 /// struct Foo<'a>(&'a u8);
@@ -131,16 +207,16 @@ pub trait Ctor {
 /// parameter.
 ///
 /// This trait is `unsafe` for the reason that it is trivial to write a type-checking
-/// implementation of `cov` that is unsound, for every type out there, by writing `panic!()`.
-/// The trait method is never actually called by the library. It's only here because it's
-/// helpful for detecting bugs.
+/// implementation of `cov` that is unsound, for every type out there, simply by writing
+/// `panic!()`. The trait method is never actually called by the library. It's only here because
+/// it's helpful for detecting bugs.
 pub unsafe trait Cov: Ctor {
     fn cov<'r, 'a, 'b>(r: &'r Self::Ty<'a>) -> &'r Self::Ty<'b>
     where
         'a: 'b;
 }
 
-/// Marker trait implemented for `Ctor`s where the constructed types are `Send`.
+/// Marker trait implemented for [`Ctor`]s where the constructed types are `Send`.
 pub trait SendCtor: Ctor {}
 impl<C> SendCtor for C
 where
@@ -149,7 +225,7 @@ where
 {
 }
 
-/// Marker trait implemented for `Ctor`s where the constructed types are `Sync`.
+/// Marker trait implemented for [`Ctor`]s where the constructed types are [`Sync`].
 pub trait SyncCtor: Ctor {}
 impl<C> SyncCtor for C
 where
@@ -158,31 +234,72 @@ where
 {
 }
 
+/// Constructor of references to `'static` values, that implements [`Ctor`] and [`Cov`].
+///
+/// This is provided for convenience, although if all you need is this, consider using the
+/// [`cryo`](https://crates.io/crates/cryo) crate, which is much more mature and compiles on
+/// stable today!
+pub struct RefCtor<T> {
+    _marker: PhantomData<T>,
+}
+
+impl<T: 'static> Ctor for RefCtor<T> {
+    type Ty<'a> = &'a T;
+}
+
+// SAFETY: &'a T is covariant over 'a when T is 'static
+unsafe impl<T: 'static> Cov for RefCtor<T> {
+    fn cov<'r, 'a, 'b>(r: &'r Self::Ty<'a>) -> &'r Self::Ty<'b>
+    where
+        'a: 'b,
+    {
+        r
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    struct Foo<'a> {
-        _marker: PhantomData<&'a ()>,
-    }
-
-    struct FooCtor;
-    impl Ctor for FooCtor {
-        type Ty<'a> = Foo<'a>;
-    }
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::time::Duration;
 
     #[test]
-    fn it_works() {
-        let leaked;
+    fn local() {
+        let answer = AtomicUsize::new(0);
+        let mut leaked;
 
         {
-            let (lifelink, _deathtouch) = link::<FooCtor>(Foo {
-                _marker: PhantomData,
-            });
-            assert!(lifelink.get().is_some());
+            let (mut lifelink, _deathtouch) = Lifelink::<RefCtor<AtomicUsize>>::new(&answer);
+            assert_eq!(
+                Some(0),
+                lifelink.get().map(|foo| foo.load(Ordering::Relaxed))
+            );
             leaked = lifelink;
         }
 
         assert!(leaked.get().is_none());
+    }
+
+    #[test]
+    fn sync() {
+        use std::sync::mpsc::channel;
+        use std::thread::spawn;
+
+        let answer = AtomicUsize::new(0);
+
+        let (mut lifelink, deathtouch) = Lifelink::<RefCtor<AtomicUsize>>::new(&answer);
+        let (send, recv) = channel();
+
+        spawn(move || {
+            let guard = lifelink.get().unwrap();
+            assert_eq!(0, guard.load(Ordering::Relaxed));
+            guard.store(42, Ordering::Release);
+            send.send(()).unwrap();
+        });
+
+        recv.recv_timeout(Duration::from_millis(20)).unwrap();
+
+        assert_eq!(42, deathtouch.unwrap().load(Ordering::Acquire));
     }
 }
